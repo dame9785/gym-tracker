49b5d4837a4be5bd0d3e6c78ab9b01c7abe2de97
David Söderberg <soderbergdavid97@gmail.com>
2026-08-08 06:49:00 +0200

    Update page.tsx

--- src/app/account/register/page.tsx
+++ src/app/account/register/page.tsx
@@ -2,10 +2,13 @@ import RegisterForm from '@/components/forms/user/register-user-form';
 import GoalService from '@/services/goal-service.ts';
 export default function Register() {
  const goals = await GoalService.getAll();
+if(!goals){
+//DTO: Do something here
+}
   return (
     <div className="container">
       <div className="form-wrapper flex justify-center items-center m-[5em]">
-        <RegisterForm />
+        <RegisterForm goals={goals}/>
       </div>
     </div>
   );
